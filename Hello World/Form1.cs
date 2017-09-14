81742d7ffedbf857a0254a53d6d8dfb1e4ca20e9
RC0025920 <RC0025920@CRO-CABLAP1-17>
2017-09-14 15:45:07 +0100

    Revert "Added goodbye in the buttin"
    This reverts commit 57c235bb5d15db1962419fa68f9a0782845a40e1.

--- Hello World/Form1.cs
+++ Hello World/Form1.cs
@@ -20,8 +20,6 @@ namespace Hello_World
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello World");
-        }MessageBox.Show("Goodbye");
-   
-
+        }
     }
 }
